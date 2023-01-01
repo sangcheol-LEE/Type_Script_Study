41d2f299b800eb6b2f2649b2aa552c442182b6f4
이상철 <zlrz6706@gmail.com>
2023-01-01 14:03:22 +0900

    객체와 클래스

--- lv3/src/index.ts
+++ lv3/src/index.ts
@@ -31,3 +31,39 @@ class Person3 {
 
 let user3 :Person3 = new Person3("Jack", 32)
 console.log(user3)
+
+let a:string, b: number;
+
+a = "이상철"
+b = 27
+
+console.log(`제 이름은 ${a}이고 나이는 ${b}입니다.`)
+
+const address : any = {
+   one : "lee",
+   two : 27,
+   city : "hwa seung",
+   num : "2222"
+}
+
+const {one , two, ...some} = address
+
+console.log(one, two, some)
+
+let coord =  {...{x : 1},...{y : 2}}
+
+console.log(coord)
+
+// 타입 단언
+interface Good {
+   name : string
+}
+
+let obj: Good = {
+   name : "good"
+}
+
+let name1 = (<Good>obj).name
+let name2 = (obj as Good).name
+
+console.log("1 : ", name1, "2 : ", name2 )
