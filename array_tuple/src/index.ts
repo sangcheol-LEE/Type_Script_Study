d9788ed720ad8823d9ba3c3743439a09a10bbb49
이상철 <zlrz6706@gmail.com>
2023-01-03 23:25:33 +0900

    5장 배열 이해하기 정리

--- array_tuple/src/index.ts
+++ array_tuple/src/index.ts
@@ -1,1 +1,29 @@
 console.log("array start")
+
+let numArray : number[] = [1,2,3,4]
+let stringArray : string[] = ["이상철은","좋은_개발자가","될_것이다"]
+
+type IPerson = {name : string, age?:number}
+let personArray :IPerson[] = [{name: "이상철"}, {name: "상철", age: 28}]
+
+console.log("numArray : ",numArray, "stringArray : ",stringArray, "personArray : ",personArray);
+
+const split = (str: string, 구분자 : string = ""):string[] => {
+	return str.split(구분자)
+}
+const join = (strArray: string[], 구분자 : string =""):string => {
+	return strArray.join(구분자)
+}
+console.log(split("ian lee", " "))
+
+console.log(join(["ian","lee"]))
+
+const numbers : number[] = [1,2,3,4,5]
+for(let i =0; i < numbers.length; i++) {
+	const item: number = numbers[i]
+	console.log(item)
+}
+
+let sampleArray: number[] = [1,2,3,4,5];
+let [first, second, third, ...rest] = sampleArray
+console.log(first, second, third, rest)
